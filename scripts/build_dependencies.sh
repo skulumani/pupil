2c395c10ec8cf6a7311e503a77863b77a8704122
Shankar Kulumani <shanks.k@gmail.com>
2018-08-27 17:23:55 -0400

    Update build process for docker

--- scripts/build_dependencies.sh
+++ scripts/build_dependencies.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+set +e
+options=("Yes" "No" "Quit")
+prompt () {
+    echo "Are you sure you want to $1"
+    select yn in "${options[@]}"; do
+        case $yn in 
+            Yes ) eval "$2";
+                break;;
+            No ) break;;
+            exit ) exit;;
+        esac
+    done
+}
+
+DOCKER_REPO=https://download.docker.com/linux/ubuntu
 
 echo "Install Docker CE for Ubuntu"
 echo "Docker is now called docker-ce (community edition)"
@@ -17,18 +32,19 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 
 # TODO Create a check to make sure that the repo is already added
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+if ! grep -q "^deb .*${DOCKER_REPO}" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+fi
 
 sudo apt-get update
-sudo apt-get install docker-ce
+prompt "Install docker" "sudo apt-get install docker-ce"
 
-read -p "Press enter to manage docker as a non-root user"
-sudo groupadd docker
-sudo usermod -aG docker $USER
+prompt "Setup docker for non-root user" "sudo groupadd docker || sudo usermod -aG docker $USER"
 
-read -p "Press enter to add docker proxy"
-sudo mkdir /etc/systemd/system/docker.service.d
-sudo cp ./scripts/http-proxy.conf /etc/systemd/system/docker.service.d
+prompt "Setup Rockwell Collins proxy for Docker" "sudo mkdir /etc/systemd/system/docker.service.d && sudo cp ./scripts/http-proxy.conf /etc/systemd/system/docker.service.d"
+
+echo "You may need to restart to get group access"
+echo "Try running docker run hello-world"
