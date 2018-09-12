1e51ddf05c47e52ffca6c28ae975f28ccb0b6e6d
Shankar Kulumani <shanks.k@gmail.com>
2018-09-12 10:09:43 -0400

    Import whole module

--- pypupil/detect_pupil.py
+++ pypupil/detect_pupil.py
@@ -10,12 +10,13 @@ Shankar Kulumani		GWU		skulumani@gwu.edu
 import cv2
 import numpy as np
 
-from circle_detector import find_pupil_circle_marker
+import circle_detector
 
 # read the image and convert to grayscale
 filename = "/tmp/combined.jpg"
 img = cv2.imread(filename)
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+cd_clss = circle_detector.CircleTracker()
 # run circle marker detector
-ellipses_list = find_pupil_circle_marker(gray_img, 1.0)
+ellipses_list = circle_detector.find_pupil_circle_marker(gray_img, 1.0)
