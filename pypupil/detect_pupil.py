b9fa99d1e83b59139b9f29a4c596006dfb4fbd62
Shankar Kulumani <shanks.k@gmail.com>
2018-09-17 08:27:27 -0400

    Move to functions

--- pypupil/detect_pupil.py
+++ pypupil/detect_pupil.py
@@ -12,30 +12,38 @@ import numpy as np
 
 import circle_detector
 
-# read the image and convert to grayscale
-filename = "/tmp/cut.jpg"
-img = cv2.imread(filename)
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+from pupil.detectors import detector_2d
 
-cd_clss = circle_detector.CircleTracker()
-# run circle marker detector
-ellipses_list = circle_detector.find_pupil_circle_marker(gray_img, 1.0)
+def threshold_example():
+    # read the image and convert to grayscale
+    filename = "/tmp/cut.jpg"
+    img = cv2.imread(filename)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-gray_img_size = gray_img.shape[::-1]
-# Resize the image
-gray_img_resize = cv2.resize(gray_img, dsize=(0, 0), fx=1.0, fy=1.0)
+    cd_clss = circle_detector.CircleTracker()
+    # run circle marker detector
+    ellipses_list = circle_detector.find_pupil_circle_marker(gray_img, 1.0)
 
-# Use three kinds of adaptive threshold to extract the edges of the image
-# The first one is for complicated scene
-# The Second one is for normal scene
-# The last one is for marker in low contrast
-img_resize_blur = cv2.GaussianBlur(gray_img_resize, (3, 3), 0.25)
-edges = [cv2.adaptiveThreshold(img_resize_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 29, 36),
-         cv2.adaptiveThreshold(img_resize_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 29, 18),
-         cv2.adaptiveThreshold(img_resize_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 29, 3)]
+    gray_img_size = gray_img.shape[::-1]
+    # Resize the image
+    gray_img_resize = cv2.resize(gray_img, dsize=(0, 0), fx=1.0, fy=1.0)
 
-edge = edges[1]
-found_pos = []
-found_size = []
-circle_clusters = circle_detector.find_concentric_circles(edge, None, None, found_pos, found_size, first_check=True, min_ellipses_num=2)
+    # Use three kinds of adaptive threshold to extract the edges of the image
+    # The first one is for complicated scene
+    # The Second one is for normal scene
+    # The last one is for marker in low contrast
+    img_resize_blur = cv2.GaussianBlur(gray_img_resize, (3, 3), 0.25)
+    edges = [cv2.adaptiveThreshold(img_resize_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 29, 36),
+            cv2.adaptiveThreshold(img_resize_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 29, 18),
+            cv2.adaptiveThreshold(img_resize_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 29, 3)]
 
+    edge = edges[1]
+    found_pos = []
+    found_size = []
+    circle_clusters = circle_detector.find_concentric_circles(edge, None, None, found_pos, found_size, first_check=True, min_ellipses_num=2)
+
+def detector_example():
+    """Try to test out the detector 2d code
+    
+    """
+    pass
