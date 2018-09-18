789dbba32bbdde2ace2aa54ef7413fa842c6a917
Shankar Kulumani <shanks.k@gmail.com>
2018-09-18 10:00:23 -0400

    Build gflags

--- scripts/build_glog.sh
+++ scripts/build_glog.sh
@@ -2,8 +2,12 @@
 
 # download and build glog from source
 GLOG_VERSION=0.3.5
+
 TEMP_DIR="$(mktemp -d)"
 INSTALL_DIR="/usr/local/"
+
+GFLAGS_VERSION="2.2.1"
+GFLAGS_URL="https://github.com/gflags/gflags/archive/v${GFLAGS_VERSION}.tar.gz"
 
 # check if the temp dir was created
 if [[ ! "$TEMP_DIR" || ! -d "$TEMP_DIR" ]]; then
@@ -33,10 +37,25 @@ else
     git checkout master
 fi
 
-read -p "Press Enter to build and install"
+read -p "Download and install gflags"
+cd ${TEMP_DIR}
+mkdir gflags
+wget ${GFLAGS_URL} -O ${TEMP_DIR}/gflags.tar.gz
+tar -xzvf gflags.tar.gz -C ./gflags --strip-components=1
+cd gflags
+mkdir build && cd build
+cmake \
+    -D BUILD_SHARED_LIBS=ON \
+    -D INSTALL_HEADERS=ON \
+    ..
+make
+sudo checkinstall make install
+
+read -p "Press Enter to build and install glog"
 
 cd "${TEMP_DIR}/glog"
 ./autogen.sh
 ./configure
 make
 sudo checkinstall make install
+
